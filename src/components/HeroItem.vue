926ed6baabbcb42aa99f437f200d85ac64f96178
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2020-04-22 17:41:23 -0700

    basic styling

---
+++ src/components/HeroItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hero-item">
+    <div class="hero-item__thumbnail">
+      <img
+        :src="`https://api.opendota.com${hero.img}`"
+        :alt="hero.localized_name"
+      />
+    </div>
+    <div class="hero-item__meta">
+      <span class="hero__name">{{ hero.localized_name }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.hero-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &__thumbnail {
+    width: 85px;
+
+    img {
+      width: 100%;
+      height: auto;
+      display: block;
+    }
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "HeroItem",
+  props: {
+    hero: Object,
+  },
+};
+</script>
--- src/components/HeroesBrowser.vue
+++ src/components/HeroesBrowser.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
-    <div v-for="hero in heroes" :key="hero.id">{{ hero.localized_name }}</div>
+  <div id="heroes-browser">
+    <hero-item v-for="hero in heroes" :key="hero.id" :hero="hero"></hero-item>
   </div>
 </template>
 
+<style lang="scss" scoped>
+#heroes-browser {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  column-gap: 10px;
+  row-gap: 10px;
+  justify-items: center;
+}
+</style>
+
 <script>
 import axios from "axios";
+import HeroItem from "components/HeroItem";
 
 export default {
   name: "HeroesBrowser",
+  components: {
+    HeroItem,
+  },
   computed: {
     heroes() {
       return this.$store.state.heroes;
