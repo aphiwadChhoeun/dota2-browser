6c78768dd63389e2c97f7017ebc2f3428f8da996
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2020-04-23 14:57:47 -0700

    load data on route changed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import Vue from "vue";
-import DataService from "services/DataService";
 
 import router from "router";
 import store from "store";
@@ -7,10 +6,5 @@ import store from "store";
 const app = new Vue({
   el: "#app",
   store: store,
-  router: router,
-  created() {
-    DataService.fetchHeroes().then((heroes) => {
-      this.$store.commit("loadHeroes", heroes);
-    });
-  },
+  router: router(store),
 });
--- src/router.js
+++ src/router.js
@@ -1,23 +1,39 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import DataService from "services/DataService";
 import HeroesBrowser from "components/HeroesBrowser";
 import HeroDetails from "components/HeroDetails";
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
-  routes: [
-    {
-      path: "/",
-      name: 'home',
-      component: HeroesBrowser,
-    },
-    {
-      path: "/hero/:id",
-      name: 'details',
-      component: HeroDetails,
-    },
-  ],
-});
+const beforeEnterMiddleware = (store) => (to, from, next) => {
+  if (from.name == null) {
+    DataService.fetchHeroes().then((heroes) => {
+      store.commit("loadHeroes", heroes);
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+const router = (store) => {
+  return new VueRouter({
+    routes: [
+      {
+        path: "/",
+        name: "home",
+        component: HeroesBrowser,
+        beforeEnter: beforeEnterMiddleware(store),
+      },
+      {
+        path: "/hero/:id",
+        name: "details",
+        component: HeroDetails,
+        beforeEnter: beforeEnterMiddleware(store),
+      },
+    ],
+  });
+};
 
 export default router;
